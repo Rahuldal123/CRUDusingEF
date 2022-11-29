97c3d88bceb6c50837ed1faa6f96ab8fe37e1e2e
Rahul <Rahul@DESKTOP-C2D05LV>
2022-11-29 14:19:53 +0530

    Add project files.

---
+++ CRUDusingEF/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUDusingEF.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDusingEF.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product>products { get; set; }
+    }
+}
---
+++ CRUDusingEF/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDusingEF.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        [Required (ErrorMessage ="Name is Required")]
+        public string Name { get; set; }
+        [Required (ErrorMessage ="Company name is required")]
+        [Display(Name="Company name")]
+
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage ="Price is required")]
+        public int  Price{ get; set; }
+    }
+}
